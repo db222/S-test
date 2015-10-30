27445c0c0d72246682fe36c0ad8512263104d1f4
Derek <derek.b26@gmail.com>
2015-10-29 18:18:21 -0700

    -changes to html structure and labeling -initial css
    Next/in progress
    -tags editting

--- public/javascripts/productEditor/productEditor.js
+++ public/javascripts/productEditor/productEditor.js
@@ -1,11 +1,12 @@
 angular.module('productManager.productEditor', []) 
 
-.controller('productEditorController', ['$rootScope', '$scope', 'Inventory', function ($rootScope, $scope, Inventory) {
+.controller('productEditorController', ['$rootScope', '$scope', 'Inventory', '$sanitize', function ($rootScope, $scope, Inventory, $sanitize) {
   $scope.product = {}
+  $scope.tags = [];
   var toUnbind = [];
   toUnbind.push($rootScope.$on('Inventory.productSelected', function(){ 
     $scope.product = jQuery.extend(true, {}, Inventory.getSelectedProduct());
-
+    $scope.tags = $scope.product.tags.split(", ");
     for(var key in $scope.product) {
       console.log('this is the key ' + key + ' this is the value ' + $scope.product[key]);
       if(key === 'options') {
@@ -19,12 +20,16 @@ angular.module('productManager.productEditor', [])
 
   toUnbind.push($rootScope.$on('productList.createNewProduct', function() {
     $scope.product = {};
+    $scope.tags = [];
   }));
 
+  $scope.addTag = function() {
 
+  }
 
   $scope.saveProduct = function() {
     if($scope.product) {
+      $scope.product.tags = $scope.tags.join(", ");
       Inventory.saveProduct($scope.product);
     }
   }
